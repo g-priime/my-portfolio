846a75b82f80079689b8d7f8606fba76ada3d978
g-priime <57049481+g-priime@users.noreply.github.com>
2020-12-22 09:26:34 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ export default function Home() {
         <main>
             <img src={image} alt="forest" className="absolute object-cover w-full h-full" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha. I'm Levon.</h1>
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    Hello. I'm Levon.</h1>
             </section>
         </main>
     )
