1833cd928727464559c180b724b01b7f24a8f61b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-28 12:21:48 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -36,7 +36,7 @@ export default function About() {
                     <div className="text-lg flex flex-col justify-center">
                         <h1 className="cursive text-6xl text-green-300 mb-4">
                             Hey there! I'm{" "}
-                            <span className="text-green-100">{author.name}</span>
+                            <span className="text-green-300">{author.name}</span>
                         </h1>
                         <div className="prose lg:prose-xl text-white">
                             <BlockContent
