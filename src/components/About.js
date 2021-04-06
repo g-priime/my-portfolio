cb7a3f2e0c6e7c79c66b5fcdbf0e331ab2bb940d
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-06 14:58:59 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,8 @@ import ice from "../ice10.jpg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
+import image from "../forest-bathing.jpg"
+
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
     return builder.image(source)
@@ -24,8 +26,8 @@ export default function About() {
     if (!author) return <div>Loading...</div>;
 
     return (
-        <main className="relative">
-            <img src={ice} alt="waterfall" className="absolute w-full" />
+        <main >
+            <img src={ice} alt="waterfall" className="absolute object-cover w-full h-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img
