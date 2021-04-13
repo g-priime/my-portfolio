61245cc37f640f2cfcf7bddafec264bbc1b3ee6d
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-13 16:13:24 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,15 @@
 import React from "react"
-import image from "../forest-bathing.jpg"
+import ice from "../ice10.jpg";
 
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="forest" className="absolute object-cover w-full h-full mt-0" />
+            <img src={ice} alt="waterfall" className="absolute object-cover w-full h-full mt-0" />
             <section className="relative flex justify-center min-h-screen md:pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                <h1 className="text-6xl text-blue-300 font-bold cursive leading-none lg:leading-snug home-name">
                     Hello. I'm Levon.</h1>
             </section>
         </main>
     )
 }
 
-/* 
-image source: https://time.com/5259602/japanese-forest-bathing/ 
-*/
