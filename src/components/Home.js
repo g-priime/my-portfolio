48b5faf398bd8ab589c1454525c5d69309176c56
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-13 15:47:09 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import image from "../forest-bathing.jpg"
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="forest" className="absolute object-cover w-full h-full" />
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+            <img src={image} alt="forest" className="absolute object-cover w-full h-full mt-0" />
+            <section className="relative flex justify-center min-h-screen md:pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
                     Hello. I'm Levon.</h1>
             </section>
