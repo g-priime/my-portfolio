d1fd5d97e6db5d8431e6fb72f0bd088c6d064e06
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-15 16:42:12 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <img src={ice} alt="waterfall" className="absolute object-cover w-full h-full mt-0" />
             <section className="relative flex justify-center min-h-screen md:pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-blue-300 font-bold cursive leading-none lg:leading-snug home-name">
+                <h1 className="text-6xl text-yellow-400 font-bold cursive leading-none lg:leading-snug home-name">
                     Hello. I'm Levon.</h1>
             </section>
         </main>
