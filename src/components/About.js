22998c1850d5d619e6d66780a49c6f9518d28a0b
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-14 20:16:39 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import sanityClient from "../client.js";
-import ice from "../ice10.jpg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
