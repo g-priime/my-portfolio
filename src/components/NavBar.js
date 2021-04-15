0d2b53409eb4e29fcf5c2431a5cd9bae8410b20c
g-priime <57049481+g-priime@users.noreply.github.com>
2021-04-15 16:42:02 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,27 +4,27 @@ import { SocialIcon } from "react-social-icons"
 
 export default function NavBar() {
     return (
-        <header className="bg-red-600">
+        <header className="bg-gray-600">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to="/" exact
                         activeClassName="text-white"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-yellow-400 hover:text-indigo-200 text-4xl font-bold cursive tracking-widest">
                         Levon
                     </NavLink>
                     <NavLink to="/post"
                         activeClassName="text-red-100 bg-red-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-yellow-400 hover:text-indigo-200">
                         Blog Posts
                     </NavLink>
                     <NavLink to="/project"
                         activeClassName="text-red-100 bg-red-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-yellow-400 hover:text-indigo-200">
                         Projects
                     </NavLink>
                     <NavLink to="/about"
                         activeClassName="text-red-100 bg-red-700"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-yellow-400 hover:text-indigo-200">
                         About Me
                     </NavLink>
                 </nav>
